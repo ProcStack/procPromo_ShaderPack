7e38e44820b1b3fc2413e97415ed38c4a0b6513e
Kevin Edzenga <trancor@metal-asylum.net>
2022-04-23 20:44:33 -0400

    Sandstone & Red Sandstone under the top level sandsterrn

--- shaders/utils/shadowCommon.glsl
+++ shaders/utils/shadowCommon.glsl
@@ -4,11 +4,11 @@
 const int shadowMapResolution = 2048; //[512 768 1024 1536 2048 3172 4096 8192 16384]
 //const float shadowMapFov = 90.0; 
 const float shadowDistance = 128.0;
-const float shadowDistanceRenderMul = 1.0; //[-1.0 1.0] Can help to increase shadow draw distance when set to -1.0, at the cost of performance
+const float shadowDistanceRenderMul = -1.0; //[-1.0 1.0] Can help to increase shadow draw distance when set to -1.0, at the cost of performance
 const float shadowIntervalSize = 1.0;
 
 const float k = 1.85;
-float a = 1.08;
+float a = 1.1;//1.08;
 
 vec4 BiasShadowProjection(in vec4 projectedShadowSpacePosition) {
   float distortFactor = log(length(projectedShadowSpacePosition.xy)+a)*k;
