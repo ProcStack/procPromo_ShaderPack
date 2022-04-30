3f25ceb1d10f1c0f1b5ea918f079067f9e424bd6
Kevin Edzenga <trancor@metal-asylum.net>
2022-04-29 23:14:25 -0400

    Check for updates in a tiny file

---
+++ GetUpdates.bat
@@ -0,0 +1,10 @@
+
+@echo off
+echo -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+echo -- Checking for any psPromo shader pack changes --
+echo -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+echo.
+git pull
+echo.
+pause
+@echo on
